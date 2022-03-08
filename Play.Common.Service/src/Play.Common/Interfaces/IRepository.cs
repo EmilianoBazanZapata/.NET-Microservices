f3a9d551000019aad1acee6e01503930c10caacf
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-03-08 18:18:26 -0300

    Agregadno una Libreria de Clases

---
+++ Play.Common.Service/src/Play.Common/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Play.Common.Interfaces
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateAsync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(Guid Id);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task RemoveAsync(Guid Id);
+        Task UpdateAsync(T entity);
+    }
+}
--- src/Play.Catalog.Service/Interfaces/IEntity.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Play.Catalog.Service.Interfaces
-{
-    public interface IEntity
-    {
-        Guid Id { get; set; }
-    }
-}
